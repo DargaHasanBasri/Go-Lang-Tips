e766a460cb0aed3d656554063c856961f9615f22
Hasan Basri <85760132+DargaHasanBasri@users.noreply.github.com>
2023-02-28 18:21:38 +0300

    Data Types
    Çoklu değişken oluşturma ve Değişkenlerin varsayılan değerleri

--- variable_declaration/main.go
+++ variable_declaration/main.go
@@ -27,8 +27,62 @@ func main() {
 	age := 40
 	isMarried := true
 
+	// çoklu değişken oluşturma
+
+	var (
+		myName      string = "Hasan"
+		myAge       int    = 22
+		myIsMarried bool   = false
+
+		myWeight float32 = 68.5
+		myHeight int     = 178
+	)
+
+	//var youName, youAge, youIsMarried, youWeight, youHeight = "Arin", 40, true, 72.5, 172
+
+	youName, youAge, youIsMarried, youWeight, youHeight := "Arin", 40, true, 72.5, 172
+
 	fmt.Println(name)
 	fmt.Println(age)
 	fmt.Println(isMarried)
+
+	fmt.Println("-----------------------")
+	fmt.Println(myName)
+	fmt.Println(myAge)
+	fmt.Println(myIsMarried)
+	fmt.Println(myHeight)
+	fmt.Println(myWeight)
+
+	fmt.Println("-----------------------")
+	fmt.Println(youName)
+	fmt.Println(youAge)
+	fmt.Println(youIsMarried)
+	fmt.Println(youHeight)
+	fmt.Println(youWeight)
+
+	fmt.Println("-----------------------")
+	fmt.Printf("%T\n", name)
+	fmt.Printf("%T\n", age)
+	fmt.Printf("%T\n", isMarried)
+
+	fmt.Println("-----------------------")
+
+	/* Zero Values --- Değişkenlerin Varsayılan Değerleri
+
+	string --->  "" boş değer döndürür
+
+	numeric ---> 0 döndürür
+
+	bool ----> false
+	*/
+
+	var temp string
+	fmt.Println(temp)
+
+	var temp2 int
+	fmt.Println(temp2)
+
+	var temp3 bool
+	fmt.Println(temp3)
 
 }
